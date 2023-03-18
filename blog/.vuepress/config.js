343e22445d9c254fe8bc68630474afa0f8c75e36
codeLove9 <1071626267@qq.com>
2023-03-18 22:32:07 +0800

    v1.0.0

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: '1980\'s Maker',
-  description: '一个出生于80年代的程序员 -- 喜爱创[客]、美[食]、动[画]、怀[旧]的新手艺人',
+  title: 'XiaoChen\'s Blog',
+  description: '一个出生于1999年的Font End Web开发程序员',
   port: 8088,
   base: '/blog/',
   markdown: {
---
+++ docs/.vuepress/.temp/internal/pagesComponents.js
@@ -0,0 +1,6 @@
+import { defineAsyncComponent } from 'vue'
+
+export const pagesComponents = {
+  // path: /404.html
+  "v-3706649a": defineAsyncComponent(() => import(/* webpackChunkName: "v-3706649a" */"C:/My Files/Documents/Study Document/interview/个人博客/vuepress-theme-maker/docs/.vuepress/.temp/pages/404.html.vue")),
+}
