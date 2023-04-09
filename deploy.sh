126d03887e2798c1cb316954ac0204bb80dc1ff8
codeLove9 <1071626267@qq.com>
2023-04-10 00:08:08 +0800

    update: 修改了部署shell脚本文件

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env sh
+# 1.运行命令： bash deploy.sh
+# 2. 在vsCode终端或者cmd中运行会抛出错误
+# 3. 需要在powerShell或者gitBash中运行
 
 # 确保脚本抛出遇到的错误
 set -e
@@ -9,9 +11,7 @@ npm run blog:build
 # 进入生成的文件夹
 cd blog/.vuepress/dist
 
-# 如果是发布到自定义域名
-#echo '80shuo.com' > CNAME
-
+# 提交到git本地暂存区
 git init
 git add -A
 git commit -m 'deploy'
@@ -20,6 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push origin gh-pages
+git push -f git@github.com:codeLove9/myBlog.git master:gh-pages
 
+# 回到原来的目录
 cd -
